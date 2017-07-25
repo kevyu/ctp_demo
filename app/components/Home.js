c64f10d44377b5dafc752818d2945ed22b7dc670
Haibin Yu <seashineyu@gmail.com>
2017-07-25 18:15:09 +0800

    update

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { List } from 'react-virtualized';
 import styles from './Home.css';
 
 export default class Home extends Component {
@@ -8,8 +9,7 @@ export default class Home extends Component {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+          <List width={600} />
         </div>
       </div>
     );
